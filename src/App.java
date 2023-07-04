936d0a059d67bf5a9b61c8a0cd60761d6b851b90
ametrey <aetcheverryrey@gmail.com>
2023-07-04 12:48:31 -0300

    solved

--- src/App.java
+++ src/App.java
@@ -5,11 +5,16 @@ public class App {
 
         int[][] tablero = new int[tamano][tamano];
 
-        for(int i = 0; i < tamano; i++){
-            for(int j = 0; j < tamano; j++){
-                
+        for (int i = 0; i < tamano; i++) {
+            for (int j = 0; j < tamano; j++) {
+
+                if((i + j) % 2 == 0){
                 tablero[i][j] = 1;
                 System.out.print(tablero[i][j]);
+                }else{
+                tablero[i][j] = 0;
+                System.out.print(tablero[i][j]);
+                }
             }
             System.out.println();
         }
